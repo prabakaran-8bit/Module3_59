9700d0c617906817818e43acd2eaed73e84d6bbd
prabakaran-8bit <73412166+prabakaran-8bit@users.noreply.github.com>
2022-03-12 03:35:03 +0530

    Add files via upload

---
+++ 2.BiCom_System/3_Implementation/main.c
@@ -0,0 +1,205 @@
+#include "MyStm32f407xx.h"
+#include <stdint.h>
+
+#define BTN_PRESSED ENABLE
+
+void car_w(void);
+void car_a(void);
+void car_b(void);
+void car_d(void);
+void enc(void);
+void enc1(void);
+void enc2(void);
+void enc3(void);
+
+void delay(void)
+{
+	for(uint32_t i=0; i<30000000; i++);
+}
+
+int main(void)
+{
+
+	GPIO_Handle_t GpioLed, GPIOBtn; 							//Initialising variables for led and blue switch
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12 ;	//Green Led
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&GpioLed);
+
+ 	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;		//Orange Led
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&GpioLed);
+
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;		//Red Led
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&GpioLed);
+
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;		//Blue Led
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&GpioLed);
+	
+	GPIOBtn.pGPIOx = GPIOA;
+    GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	GPIO_PeriClockControl(GPIOA, ENABLE);
+	GPIO_Init(&GPIOBtn);
+
+	while(1)
+	{
+		enc();				//encryption for window function
+
+		enc1();				//encryption for alarm function
+
+		enc2();				//encryption for car_battery function
+
+		enc3();				//encryption for door function
+
+	}
+
+}
+
+void enc(void)
+{
+	p1:if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)== BTN_PRESSED)
+	{
+		car_w();
+	}
+	else
+		goto p1;
+
+}
+
+void enc1(void)
+{
+	uint32_t i=0, count = 1;
+	for(i=0;i<2;i++)
+	{
+	if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)== BTN_PRESSED)
+		count++;
+	else
+		i=0;
+	}
+	if(count==2)
+		{
+
+			car_a();
+
+		}
+}
+
+void enc2(void)
+{
+	uint32_t i=0, count = 1;
+	for(i=0;i<3;i++)
+	{
+	if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)== BTN_PRESSED)
+		count++;
+	else
+		i=0;
+	}
+	if(count==3)
+		{
+
+			car_b();
+
+		}
+}
+
+void enc3(void)
+{
+	uint32_t i=0, count = 1;
+	for(i=0;i<4;i++)
+	{
+	if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)== BTN_PRESSED)
+		count++;
+	else
+		i=0;
+	}
+	if(count==4)
+		{
+
+			car_d();
+
+		}
+}
+
+
+
+
+
+void car_w(void)
+{
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+}
+
+void car_a(void)
+{
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, 0);
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, 0);
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, 0);
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, 0);
+}
+
+void car_b(void)
+{
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, 0);
+}
+
+void car_d(void)
+{
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, 0);
+}
+//makefile,startup and linker file done by prabakaran
+//car window ,alarm, battery door are done by Prabakaran and Shafi through discussion in meet call
